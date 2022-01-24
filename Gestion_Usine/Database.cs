b3b101d3cf79aa24323803873cbcca2b43ffca91
Ilyass Elkhadiri <elkhadiri.iilyass@ofppt-edu.ma>
2022-01-24 12:06:58 +0100

    create class database

---
+++ Gestion_Usine/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Gestion_Usine
+{
+    class Database
+    {
+        public SqlConnection db = new SqlConnection(@"Data Source=DESKTOP-J5SB1HI;Initial Catalog=Usine;Integrated Security=True");
+        public SqlCommand cmd = new SqlCommand();
+        public SqlDataReader Reader;
+        public SqlDataAdapter Adapter;
+        public DataSet Set = new DataSet();
+        public DataTable dt = new DataTable();
+        public DataRow Row;
+        public SqlCommandBuilder Builder;
+
+        public void Connection()
+        {
+            if (db.State.Equals(ConnectionState.Closed) || db.State.Equals(ConnectionState.Broken))
+            {
+                db.Open();
+            }
+            return;
+        }
+
+        public void Disconnection()
+        {
+            if (db.State.Equals(ConnectionState.Open))
+            {
+                db.Close();
+            }
+            return;
+        }
+    }
+}
